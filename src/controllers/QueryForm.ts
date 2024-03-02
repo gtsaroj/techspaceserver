6c946e3db707bd457cce4f35b1b68cff56a85d80
gtsaroj <sarojgt326@gmail.com>
2024-03-02 23:50:28 +0545

    complete gmail integrate && firebase iniliaze

---
+++ src/controllers/QueryForm.ts
@@ -0,0 +1,38 @@
+import express from "express";
+import { AsyncHandler } from "../utils/Asynhandler.js";
+import { render } from "@react-email/render";
+import { transportor } from "../email/email.js";
+import QueryForm from "../emails/QueryForm.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { QueryContactType } from "../models/QueryContactTypes.js";
+
+export const SubmitQueryForm = AsyncHandler(
+  async (req: express.Request, res: express.Response) => {
+    const responseData = req.body;
+
+    const contactDetails = responseData?.find(
+      (contact: QueryContactType) => contact.fullname
+    );
+
+    const renderHtml = render(QueryForm(contactDetails, responseData));
+
+    const emailForm = {
+      from: process.env.EMAIL,
+      to: contactDetails.email,
+      subject: "QueryForm of User",
+      html: renderHtml,
+    };
+
+    transportor.sendMail(emailForm, (err, success) => {
+      if (!success) {
+        console.log(`Error occured while sending : ${err}`);
+        return new ApiError("Your message failed", 400, err?.message);
+      }
+      console.log(`Succesfully : ${success}`);
+      return res
+        .status(200)
+        .json(new ApiResponse(200, "message was sent successfully"));
+    });
+  }
+);
---
+++ src/db/firebase.ts
@@ -0,0 +1,26 @@
+import {  initializeApp } from "firebase-admin/app";
+import admin from "firebase-admin"
+import { getFirestore, Firestore } from "firebase-admin/firestore";
+import config from "./config.js";
+
+import ServiceAccount  from "../../techspace-9t9k-firebase-adminsdk-vtagy-708ba868ca.json" assert {type: "json"};
+
+
+const ConnectFirbaseDB = async (): Promise<Firestore | undefined> => {
+  try {
+    initializeApp({
+      
+      ...config.firebaseConfig,
+      credential: admin.credential.cert(ServiceAccount as any)
+    });
+    const firebase = getFirestore();
+    return firebase;
+  } catch (error) {
+    console.error(`Failed to connect while connecting ${error}`);
+  }
+};
+
+const Database = async() => {
+  await  ConnectFirbaseDB();
+} 
+export { ConnectFirbaseDB, Database };
